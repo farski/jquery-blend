62893840415cb533ef042d9b1e24c9c189d42bca
Chris Kalafarski <chris@farski.com>
2012-02-19 19:38:25 -0500

    Soft light :'(

--- jquery.blend.js
+++ jquery.blend.js
@@ -21,7 +21,13 @@
       lineardodge: function(base, adj) { return Math.min((base + adj), 255); },
       // 
       overlay: function(base, adj) { return (base < 128) ? ((2 * base * adj) / 255) : (255 - (2 * (255 - base) * (255 - adj) / 255)); },
-      softlight: function(base, adj) { return (base < 128) ? ((base * adj) / 255) : (255 - ((255 - base) * (255 - adj) / 255)); },
+      softlight: function(base, adj) {
+        if (adj < 128) {
+          return (2 * base * adj) / 255;
+        } else {
+          return 255 - ((2 * (255 - base) * (255 - adj)) / 255);
+        }
+      },
       hardlight: function(base, adj) { return adj < 128 ? (2 * base * adj) / 255 : 255 - ((2 * (255 - base) * (255 - adj)) / 255); },
       //
       difference: function(base, adj) { return Math.abs(base - adj); },
